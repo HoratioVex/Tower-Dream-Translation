7d6e18998679eb9e1a4095e534f1fb5053657368
HoratioVex <38878728+HoratioVex@users.noreply.github.com>
2023-11-09 20:57:26 +0100

    Add files via upload

--- Graphics/ps_InsertFont.ps1
+++ Graphics/ps_InsertFont.ps1
@@ -5,15 +5,14 @@ $infile = Get-Item "Font_Latin.bin"
 $input = [System.IO.File]::ReadAllBytes($infile.FullName)
 
 $filestream = $romfile.OpenWrite()
-$filepos = 0x030B0A #Letter A
+$filepos = 0x030A56 #Letter 0
 $filestream.Seek($filepos, [System.IO.SeekOrigin]::Begin)
 
-foreach ($i in 0..(26*2+4))
+foreach ($i in 0..(($input.Count/16)-1))
 {
 	$filestream.Write($input,$i*16,16)
 	$filestream.WriteByte(0xFF)
 	$filestream.WriteByte(0xFF)
-	
 }
 $filestream.Dispose()
 write-host "Done"
